76e98177db7ac1f070b1dbc64b3f449daa2c47ee
ImIshan7 <ishandanu999@gmail.com>
2024-02-19 15:05:31 +0530

    Content Updated - About

--- view/Home.tsx
+++ view/Home.tsx
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import background from '../images/background.jpeg';
+import './style.css';
 
 export class Home extends Component {
     render() {
@@ -8,11 +8,11 @@ export class Home extends Component {
                 <nav className="bg-black text-white py-3 px-4 flex items-center justify-between">
                     <a className="font-extrabold text-yellow-400 text-xl tracking-tight" href="#">Ishan</a>
                     <div className="flex items-center">
-                        <a className="text-lg font-bold  px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Home</a>
-                        <a className="text-lg font-bold  px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">About</a>
-                        <a className="text-lg font-bold  px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Project</a>
-                        <a className="text-lg font-bold  px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Service</a>
-                        <a className="text-lg font-bold  px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Contact</a>
+                        <a className="text-xl font-bold font-mono  px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Home</a>
+                        <a className="text-xl font-bold font-mono px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">About</a>
+                        <a className="text-xl font-bold font-mono px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Project</a>
+                        <a className="text-xl font-bold font-mono px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Service</a>
+                        <a className="text-xl font-bold font-mono px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Contact</a>
                     </div>
                 </nav>
 
@@ -21,7 +21,9 @@ export class Home extends Component {
                         <div className="text-center">
                             <p
                                 className="max-w-4xl mx-auto mb-4 text-6xl font-bold leading-tight text-white sm:leading-tight sm:text-5xl lg:text-6xl lg:leading-tight">
-                                Full Stack Developer
+                                Full Stack Developer <br />
+                                Entrepreneur<br/>
+                                Digital Creator
                             </p>
                             <h1 className="max-w-2xl mx-auto px-6 text-lg text-white font-inter">
                                 Give The Best Things For You and Modern Work Experience For Your Business
@@ -41,12 +43,64 @@ export class Home extends Component {
                     <div className="bg-black">
                         <div className="relative mx-auto mt-4 md:mt-8">
                             <div className="lg:max-w-4xl lg:mx-auto">
-                                <img className="px-4 md:mpx-8" src="https://images.unsplash.com/photo-1628277613967-6abca504d0ac" />
+                                <img className="px-4 md:mpx-8 duration-500 hover:scale-105 hover:shadow-xl" src="https://images.unsplash.com/photo-1628277613967-6abca504d0ac" />
                             </div>
                         </div>
                     </div>
 
                 </section>
+
+                <div className="px-2 py-20 w-full flex justify-center bg-black">
+                    <div className="bg-white lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg rounded-lg">
+                        <div className="lg:w-1/2">
+                            <div id="backgroundimage" className="lg:scale-110 h-80 bg-cover lg:h-full rounded-b-none border lg:rounded-lg">
+                            </div>
+                        </div>
+                        <div className="py-12 px-6 lg:px-12 max-w-xl lg:max-w-5xl lg:w-1/2 rounded-t-none border lg:rounded-lg">
+                            <h2 className="text-5xl text-gray-800 font-extrabold">
+                                Who We Are ?
+                            </h2>
+                            <p className="mt-4 font-bold text-gray-950">
+
+                                Ishan Dhananjana: A Journey of Passion
+
+                                In the charming city of Galle, Sri Lanka, Ishan Dhananjana's story unfolds with a vibrant
+                                tapestry of passion and purpose. Born on February 20, 2001, Ishan's journey is a testament
+                                to resilience and determination. From his early fascination with technology to his freelance
+                                endeavors, Ishan's portfolio reflects his creativity and commitment. His narrative inspires
+                                others to embrace their dreams and persevere against all odds. With each step forward,
+                                Ishan continues to carve his path, driven by his unwavering love for innovation and
+                                storytelling.
+
+                            </p>
+
+                            <div className=" flex justify-center mt-5 space-x-5">
+                                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+                                </a>
+                                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
+                                </a>
+                                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
+                                </a>
+                                <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
+                                </a>
+                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
+                                </a>
+
+                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/fluent/30/000000/github.png" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
 
 
 
